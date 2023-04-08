e78eab599a00312a18a3c70c7db41137c1cf97be
Rattapon-Insa <rattapon.insa@gmail.com>
2023-04-08 12:44:27 +0700

    finalize api code

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,17 +1,30 @@
 import json
-import numpy as np
+import uuid
+
+GET_RAW_PATH = '/getPerson'
+CREAT_RAW_PATH = '/createPerson'
 
 
 def lambda_handler(event, context):
-    first_name = event['first_name']
-    last_name = event['last_name']
-    try:
+    if event['rawPath'] == GET_RAW_PATH:
+        # do something
+        print("this is get path")
+        personId = event['queryStringParameters']['personId']
+        print("Recieve rquest for person id = ", personId)
         return {
-            "statusCode": 200,
-            "body": json.dumps({
-                "message": f"Hi {first_name} {last_name} ",
-            }),
+            "personId" : str(personId),
+            "firstName" : "Rattapon",
+            "lastName" : "Ins"
         }
-    except:
-        return f'error in input {first_name} or {last_name}'
+    elif event['rawPath'] == CREAT_RAW_PATH:
+        print("this is create path")
+        decodeEvent = json.loads(event['body'])
+        firstName = decodeEvent['firstName']
+        lastName = decodeEvent['lastName']
+        return {
+                "personId" : str(uuid.uuid1()),
+                "firstName" : firstName,
+                "lastName" : lastName,
+                "status" : "created"
+                }
 
